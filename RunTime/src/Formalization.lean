acf4dfb7416998289096d3a0b512c83e1c3b385b
Tomaz Gomes <tomgm1502@gmail.com>
2021-01-25 13:40:25 -0300

    Added proof for ordered_insert's time complexity

--- RunTime/src/Formalization.lean
+++ RunTime/src/Formalization.lean
@@ -1,13 +1,16 @@
 import data.list.sort
-import data.int.basic
 
 variables {α : Type} (r : α → α → Prop) [decidable_rel r]
 local infix ` ≼ ` : 50 := r
 
+@[simp] def go (b : α) (l : list α × ℕ) : (list α × ℕ) := (b :: l.fst, l.snd + 1)
+
+-- the second value in the output is the number of comparisons made
 @[simp] def ordered_insert (a : α) : list α → (list α × ℕ)
 | [] := ([a], 0)
 | (b :: l) := if a ≼ b then (a :: b :: l, 1)
-                       else let (l', n) := ordered_insert l in (b :: l', n + 1)
+                       else go b (ordered_insert l)
+                       -- else let (l', n) := ordered_insert l in (b :: l', n + 1)
 
 @[simp] def insertion_sort : list α → (list α × ℕ)
 | [] := ([], 0)
@@ -15,10 +18,16 @@ local infix ` ≼ ` : 50 := r
               in (l'', n + m)
 
 
--- theorem ordered_insert_linear (a : α) : ∀ l : list α, (ordered_insert r a l).snd ≤ l.length :=
--- begin
---   intros,
---   induction l,
---   { simp, },
---   {}
---  end
+theorem ordered_insert_linear (a : α) : ∀ l : list α, (ordered_insert r a l).snd ≤ l.length
+| [] := by simp
+| (b :: l) :=
+begin
+  simp,
+  split_ifs,
+  { simp, },
+  { have ih : (ordered_insert r a l).snd + 1 ≤ l.length + 1 := nat.pred_le_iff.mp (ordered_insert_linear l),
+    refine le_trans _ ih,
+    refine eq.ge _,
+    exact rfl,
+  }
+end
